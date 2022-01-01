79bbfa9946429629c716ec39c27dea6cfa4695ed
Darkness <alimurtaza7x@gmail.com>
2022-01-01 12:23:40 +0330

    main website

---
+++ pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <>
+        <h1 style={{alignItems:"center", justifyContent:"center" , display:"flex"}}>Helooooo</h1>
+        </>
+     );
+}
+ 
+export default Home;
